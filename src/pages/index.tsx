ae4d12bbb4d327e13b25fa6cf4dfd14c1785b21e
jimanPark <im@gman.io>
2020-03-31 23:39:32 +0900

    [feature] added modal

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
+import { useState, FC } from 'react';
 import styled from "styled-components";
+import { Modal, withStyles, Fade, Backdrop } from '@material-ui/core';
 
 const WrapperIndexPage = styled.div`
   min-height: 100%;
@@ -7,9 +9,45 @@ const WrapperIndexPage = styled.div`
   justify-content: center;
 `;
 
+const CustomModal = withStyles({
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})(Modal);
+
 const Index = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <WrapperIndexPage>Thinking...</WrapperIndexPage>
+    <>
+      <WrapperIndexPage onClick={handleOpen}>Thinking...</WrapperIndexPage>
+      <CustomModal
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <>
+            <h2 id="transition-modal-title">Transition modal</h2>
+            <p id="transition-modal-description">react-transition-group animates me.</p>
+          </>
+        </Fade>
+      </CustomModal>
+    </>
   );
 }
 
